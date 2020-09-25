68e8d1f16e8d84054ee68185699d8dbc546d4e07
Sarah Abdelkhalek <sabdelkhalek@google.com>
2020-09-25 17:30:55 +0200

    added dimention input

--- timeline-dep-graph/src/app/timeline-dep-graph/timeline/timeline.component.ts
+++ timeline-dep-graph/src/app/timeline-dep-graph/timeline/timeline.component.ts
@@ -1,7 +1,4 @@
-import {
-  AfterViewInit, ChangeDetectorRef,
-  Component, ElementRef, ViewChild
-} from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { DataSet, Timeline, TimelineOptions } from 'vis';
 
 @Component({
@@ -18,22 +15,53 @@ export class TimelineComponent implements AfterViewInit {
 
   constructor(private readonly cdRef: ChangeDetectorRef) { }
 
+  /**
+   * The height/width of the timeline in pixels or as a percentage.
+   * When height is undefined or null,
+   * the height of the timeline is automatically adjusted to fit the contents.
+   */
+  @Input() height: number | string;
+  @Input() width: number | string;
+  @Input() maxHeight: number | string;
+  @Input() minHeight: number | string;
+
   ngAfterViewInit(): void {
     this.renderTimeline();
   }
 
   private renderTimeline(): void {
-    const timelineStart = new Date();
-    timelineStart.setDate(timelineStart.getDate() - 1);
-
-    // Set the start of the timeline to one day before the current time.
-    const timelineOptions: TimelineOptions = { start: timelineStart.getTime() };
+    const timelineOptions = this.getTimelineOptions();
 
     this.timeline = new Timeline(
       this.timelineVis.nativeElement,
       this.items,
       timelineOptions);
     this.cdRef.detectChanges();
+  }
+
+  private getTimelineOptions(): TimelineOptions {
+    const timelineStart = new Date();
+    timelineStart.setDate(timelineStart.getDate() - 1);
+
+    // Set the start of the timeline to one day before the current time.
+    const timelineOptions: TimelineOptions = {
+      start: timelineStart.getTime(),
+    };
+
+    if (this.height) {
+      timelineOptions.height = this.height;
+    }
+    if (this.width) {
+      timelineOptions.width = this.width;
+    }
+    if (this.maxHeight) {
+      timelineOptions.maxHeight = this.maxHeight;
+    }
+    if (this.minHeight) {
+      timelineOptions.minHeight = this.minHeight;
+    }
+
+    return timelineOptions;
   }
 
 }
