1e10fda4c941a046b2689f1bada1699c32720f81
Sarah Abdelkhalek <sabdelkhalek@google.com>
2020-09-25 16:26:56 +0200

    max line length

--- timeline-dep-graph/src/app/timeline-dep-graph/timeline/timeline.component.ts
+++ timeline-dep-graph/src/app/timeline-dep-graph/timeline/timeline.component.ts
@@ -1,4 +1,7 @@
-import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  AfterViewInit, ChangeDetectorRef,
+  Component, ElementRef, ViewChild
+} from '@angular/core';
 import { DataSet, Timeline, TimelineOptions } from 'vis';
 
 @Component({
@@ -13,20 +16,23 @@ export class TimelineComponent implements AfterViewInit {
 
   @ViewChild('timelineVis', { static: true }) timelineVis: ElementRef;
 
-  constructor(private readonly cdRef: ChangeDetectorRef) {}
+  constructor(private readonly cdRef: ChangeDetectorRef) { }
 
   ngAfterViewInit(): void {
     this.renderTimeline();
   }
 
-  private renderTimeline(): void{
+  private renderTimeline(): void {
     const timelineStart = new Date();
     timelineStart.setDate(timelineStart.getDate() - 1);
 
     // Set the start of the timeline to one day before the current time.
-    const timelineOptions: TimelineOptions = {start: timelineStart.getTime()};
+    const timelineOptions: TimelineOptions = { start: timelineStart.getTime() };
 
-    this.timeline  = new Timeline(this.timelineVis.nativeElement, this.items, timelineOptions);
+    this.timeline = new Timeline(
+      this.timelineVis.nativeElement,
+      this.items,
+      timelineOptions);
     this.cdRef.detectChanges();
   }
 
